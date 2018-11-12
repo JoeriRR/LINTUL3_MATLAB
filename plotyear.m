075c466f0c31c7ead92cdf084c9241e26cfb9247
s168210 <s168210@s168210.campus.tue.nl>
2018-11-12 09:40:54 +0100

    not sure anything changed but ok

--- plotyear.m
+++ plotyear.m
@@ -1,5 +1,4 @@
 function plotyear(DOY,data1,data2,data3)
-
 plot(DOY,data1,'LineWidth',2)
 if nargin >= 3
     hold on;
@@ -8,9 +7,7 @@ if nargin >= 3
         plot(DOY,data3,'LineWidth',2);
     end
     hold off;
-end
-
-    
+end    
     xlim([1 365]);grid on;
     set(gca,'XTick',[1,32,60,91,121,152,182,213,244,274,305,335])
     set(gca,'XTickLabel',{'            Jan','            Feb','            Mar',...
